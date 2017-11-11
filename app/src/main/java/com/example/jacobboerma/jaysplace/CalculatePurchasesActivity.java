e6c98ccb7f5b957bb93e6b344cac24a77b15296a
JBoerma <jacobswb2@gmail.com>
2017-11-10 22:00:17 -0800

    Created transitions between the 4 activities. MainActivity | OptionsActivity | Enter Flex - Calculate Food

---
+++ app/src/main/java/com/example/jacobboerma/jaysplace/CalculatePurchasesActivity.java
@@ -0,0 +1,20 @@
+package com.example.jacobboerma.jaysplace;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class CalculatePurchasesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_purchases);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
---
+++ app/src/main/java/com/example/jacobboerma/jaysplace/OptionActivity.java
@@ -0,0 +1,55 @@
+package com.example.jacobboerma.jaysplace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class OptionActivity extends AppCompatActivity {
+    final static int TIME_OUT = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_option);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        final Button calculateButton = findViewById(R.id.calculate_button);
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent myIntent = new Intent(OptionActivity.this, CalculatePurchasesActivity.class);
+                        startActivity(myIntent);
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    }
+                }, TIME_OUT);
+            }
+        });
+
+        final Button flexButton = findViewById(R.id.enter_flex_button);
+        flexButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Code here executes on main thread after user presses button
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent myIntent = new Intent(OptionActivity.this, EnterFlexActivity.class);
+                        startActivity(myIntent);
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                    }
+                }, TIME_OUT);
+            }
+        });
+    }
+}
